00d77b986c88424198e7067067a275764a8e4b88
David <idavid@fastmail.com>
2019-12-19 12:19:50 -0600

    removed commented code.

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,39 +9,10 @@ class User
     protected $lastName;
     protected $email;
 
-    // public function __set($name, $value)
-    // {
-    //     $this->data[$name] = $value;
-    // }
-
-    // public function __get($name)
-    // {
-    //     if (array_key_exists($name, $this->data)) {
-    //         return $this->data[$name];
-    //     }
-
-    //     $trace = debug_backtrace();
-    //     trigger_error(
-    //         'Undefined property via __get(): ' . $name .
-    //         ' in ' . $trace[0]['file'] .
-    //         ' on line ' . $trace[0]['line'],
-    //         E_USER_NOTICE);
-    //     return null;
-    // }
-
-    // public function __call($name, $arguments) {
-    //     //do a get
-    //     if (preg_match('/^get(.+)/', $name, $matches)) {
-    //         $var_name = $matches[1];
-    //         return $this->__get($var_name);
-    //     }
-    //     //do a set
-    //     if (preg_match('/^set(.+)/', $name, $matches)) {
-    //         $var_name = $matches[1];
-    //         $this->__set($var_name, $arguments[0] );
-    //     }
-    // }
-
+    /**
+     * Abilty to set firstname
+     * @param string $firstName
+     */
     public function setFirstName($firstName)
     {
         $this->firstName = trim($firstName);
